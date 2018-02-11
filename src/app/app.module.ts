dea2ebda4897746e06f183a5ef794c503d2ae70a
svoice@gmail.com <svoice@gmail.com>
2018-02-11 16:34:20 +0100

    Se añade un componente simple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SimpleComponentComponent } from './simple-component/simple-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleComponentComponent
   ],
   imports: [
     BrowserModule
